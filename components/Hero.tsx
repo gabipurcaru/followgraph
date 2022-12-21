6a97796fb29dda44cc245a697bd10ad3232c03bc
Gabi Purcaru <gabi@purcaru.com>
2022-12-21 08:02:17 +0000

    fix padding

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 export default function Hero({ }) {
   return <section className="bg-white dark:bg-gray-900">
-    <div className="grid max-w-screen-xl px-4 pt-20 pb-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12 lg:pt-28">
+    <div className="grid max-w-screen-xl px-4 pt-20 pb-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12 lg:pt-28 lg:px-20">
       <div className="mr-auto place-self-center lg:col-span-7">
         <h1 className="max-w-2xl mb-4 text-4xl font-extrabold leading-none tracking-tight md:text-5xl xl:text-6xl dark:text-white">
           Find awesome people <br /> on Mastodon.
