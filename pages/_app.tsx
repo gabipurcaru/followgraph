b7dd886f9e84f73a6112009127525062011551a7
Gabi Purcaru <gabi@purcaru.com>
2022-12-21 08:39:31 +0000

    oops

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,8 +3,8 @@ import { Analytics } from '@vercel/analytics/react';
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
-  <>
+  return (<>
     <Component {...pageProps} />
     <Analytics />
-  </>
+  </>);
 }
