7a2626fbc13386d5012488908001d1854c27d207
Steffen Weitz <weitz.steffen@gmail.com>
2022-12-24 22:13:52 +0100

    Add prettier package and apply config to all files

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,12 @@
 import '../styles/globals.css'
-import { Analytics } from '@vercel/analytics/react';
+import { Analytics } from '@vercel/analytics/react'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return (<>
-    <Component {...pageProps} />
-    <Analytics />
-  </>);
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  )
 }
