07b905cd45bb0f40d4f4287b9503854f118eb0d4
Gabi Purcaru <gabi@purcaru.com>
2022-12-24 16:32:42 +0100

    change meta description

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,7 +10,7 @@ export default function Home() {
     <>
       <Head>
         <title>Followgraph for Mastodon</title>
-        <meta name="description" content="Find people to follow by expanding your follow graph." />
+        <meta name="description" content="Find people to follow on Mastodon by expanding your follow graph." />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
